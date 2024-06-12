8af49160cf22773d2348d61b24b4cb22f41a7239
Justin Steffen <jcsteffen03@gmail.com>
2024-06-11 21:24:24 -0600

    Finished Delieverables

--- src/index.js
+++ src/index.js
@@ -1,24 +1,65 @@
-// index.js
+
+document.addEventListener('DOMContentLoaded', () => {
+  fetch("http://localhost:3000/ramens")
+  .then((r)=> r.json())
+  .then(ramens => {
+    main(ramens)
+  })
+  .catch(error => console.error('Error fetching the ramens:', error));
+})
 
 // Callbacks
 const handleClick = (ramen) => {
-  // Add code
+  const ramenImage = document.querySelector(`.detail-image`)
+  const name = document.querySelector(`.name`)
+  const restaurant = document.querySelector(`.restaurant`)
+  const rating = document.querySelector('#rating-display')
+  const comment = document.querySelector('#comment-display')
+
+  ramenImage.src = ramen.image
+  name.textContent = ramen.name
+  restaurant.textContent = ramen.restaurant
+  rating.textContent = ramen.rating 
+  comment.textContent = ramen.comment
+
 };
 
 const addSubmitListener = () => {
-  // Add code
+  const ramenForm = document.querySelector("#new-ramen")
+  ramenForm.addEventListener("submit",(e)=> {
+    e.preventDefault()
+
+    const newRamen = {
+
+      image: e.target["new-image"].value,
+      name: e.target["new-name"].value,
+      restaurant: e.target["new-restaurant"].value,
+      rating: e.target["new-rating"].value,
+      comment: e.target["new-comment"].value
+    }
+    displayRamens(newRamen)
+
+  })
 }
 
-const displayRamens = () => {
-  // Add code
+function displayRamens(ramen){
+  const ramenImg = document.createElement("img")
+  ramenImg.src = ramen.image
+  const menuOption = document.querySelector("#ramen-menu")
+  menuOption.append(ramenImg)
+
+  ramenImg.addEventListener("click",() => {
+  handleClick(ramen)
+
+  })
 };
 
-const main = () => {
-  // Invoke displayRamens here
-  // Invoke addSubmitListener here
+const main = (ramens) => {
+  ramens.forEach((ramen)=> {
+    displayRamens(ramen)
+  })
+  addSubmitListener()
 }
-
-main()
 
 // Export functions for testing
 export {
